310f370f75affdb6b9b123e5f5f04fcea9c52f40
borysl <borys.lebeda@gmail.com>
2021-12-22 13:01:12 +0200

    Analyse attachments

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const ERRORS = {
   SOME_ERROR: 99,
 };
 
+const { columns, validateTodo } = require('./trelloSettings.js');
+
 if (process.env.NODE_ENV !== 'production') {
   console.log('Loading env from local file in non-production environment.');
   const dotenvParse = require('dotenv').config();
@@ -17,30 +19,6 @@ const port = process.env.PORT || 8080;
 
 var Trello = require('trello');
 var trello = new Trello(process.env.TRELLO_APP_KEY, process.env.TRELLO_API_TOKEN);
-
-/*
-displayBoards(process.env.TRELLO_USER_ID);
-function displayBoards(user) {
-  trello.getBoards(user, (err, boards) => {
-    handleError(err);
-    boards.forEach(board => {
-      console.log(`Board ${board.name}(${board.id})`);
-    });
-  });
-}
-
-function getListsOnBoard(boardId) {
-  return new Promise((resolve, reject) => {
-    trello.getListsOnBoard(boardId, null, (err, lists) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(lists);
-      }
-    });
-  });
-}
-*/
 
 function handleError(err, res) {
   if (err) {
@@ -56,30 +34,9 @@ function handleError(err, res) {
   }
 }
 
-function validateToDo(card) {
-  let sins = [];
-  if (card.labels.length === 0) sins.push('No labels for classification!');
-  if (!card.desc || card.checklists.length < 0) sins.push('Neither description nor checklist!');
-
-  if (card.labels.find(_ => _.name === 'FE')) {
-    if (card.attachments.length === 0) sins.push('No image attached to FE task!');
-  }
-
-  if (card.labels.find(_ => _.name === 'Bug')) {
-    if (!card.desc.includes('CONDITION')) sins.push('Bug contains no CONDITION');
-    if (!card.desc.includes('ACTION')) sins.push('Bug contains no ACTION');
-    if (!card.desc.includes('EXPECTED')) sins.push('Bug contains no EXPECTED');
-    if (!card.desc.includes('ACTUAL')) sins.push('Bug contains no ACTUAL');
-    if (!card.desc.match(/\d+\.\d+\.\d+\.\d+/)) sins.push('Bug contains no version');
-  }
-  return sins;
-}
-
 const http = require('http');
 
-const server = http.createServer(async (req, res) => {
-  let lists = await trello.getListsOnBoard(process.env.TRELLO_BOARD_ID);
-  let cards = await trello.getCardsOnList(lists[2].id);
+async function populateCards(cards) {
   let populatePromises = cards.map(async card => {
     let checkListPromise = trello.getChecklistsOnCard(card.id);
     let attachmentPromise = trello.getAttachmentsOnCard(card.id);
@@ -89,20 +46,34 @@ const server = http.createServer(async (req, res) => {
   });
 
   await Promise.all(populatePromises);
+}
 
-  let html =
-    '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Trello Shame</title><body><h1>Input Quality Bar for our <a href="https://trello.com/b/VkJo4Kd7/website">Trello</a></h1>';
-  cards.forEach(card => {
-    let sins = validateToDo(card);
-    html += `<li>Card <a href="${card.url}">${card.name}</a> ${sins.length == 0 ? 'is OK' : sins.join(', ')}. </li>`;
-  });
-  html += '</body></html>';
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Content-Length': html.length,
-    Expires: new Date().toUTCString(),
-  });
-  res.end(html);
+const server = http.createServer(async (req, res) => {
+  try {
+    let boards = await trello.getBoards(process.env.TRELLO_USER_ID);
+    let board = boards.find(_ => _.id === process.env.TRELLO_BOARD_ID);
+    let lists = await trello.getListsOnBoard(board.id);
+    var todoList = lists[columns['todo']];
+    let todoCards = await trello.getCardsOnList(todoList.id);
+
+    await populateCards(todoCards);
+
+    let html = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Trello Shame</title><body>';
+    html += `<h1>Dynamic Quality Bars for our <a href="${board.url}">Trello</a></h1>`;
+    todoCards.forEach(card => {
+      let sins = validateTodo(card);
+      html += `<li>Card <a href="${card.url}">${card.name}</a> ${sins.length == 0 ? 'is OK' : sins.join(', ')}. </li>`;
+    });
+    html += '</body></html>';
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Content-Length': html.length,
+      Expires: new Date().toUTCString(),
+    });
+    res.end(html);
+  } catch (err) {
+    handleError(err, res);
+  }
 });
 
 server.listen(port);
---
+++ trelloSettings.js
@@ -0,0 +1,34 @@
+function validateTodo(card) {
+  let sins = [];
+  if (card.labels.length === 0) sins.push('no labels for classification');
+  if (!card.desc && card.checklists.length === 0) sins.push('neither description nor checklist');
+
+  if (card.labels.find(_ => _.name === 'FE')) {
+    if (card.attachments.length === 0) {
+      sins.push('No image attached to FE task!');
+    } else {
+      if (!card.attachments.find(_ => _.url.startsWith('https://www.figma.com/file/')))
+        sins.push('no Figma reference attached to FE task');
+      if (!card.attachments.find(_ => _.fileName && (_.fileName.endsWith('.png') || _.fileName.endsWith('.jpg'))))
+        sins.push('no screenshot attached to FE task');
+    }
+  }
+
+  if (card.labels.find(_ => _.name === 'Bug')) {
+    if (!card.desc.includes('CONDITION')) sins.push('bug contains no CONDITION');
+    if (!card.desc.includes('ACTION')) sins.push('bug contains no ACTION');
+    if (!card.desc.includes('EXPECTED')) sins.push('bug contains no EXPECTED');
+    if (!card.desc.includes('ACTUAL')) sins.push('bug contains no ACTUAL');
+    if (!card.desc.match(/\d+\.\d+\.\d+\.\d+/)) sins.push('bug contains no version');
+  }
+  return sins;
+}
+
+module.exports = {
+  columns: {
+    todo: 2,
+    wip: 3,
+    resolved: 4,
+  },
+  validateTodo,
+};
